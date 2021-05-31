cee57cee98a2be1e8c4f24a9b7a05d7987f9368f
Liza Good <GirlKiller512@users.noreply.github.com>
2021-06-01 02:32:28 +0800

    Update neteasedown.py

--- neteasedown.py
+++ neteasedown.py
@@ -1,25 +1,21 @@
 from hoshino import Service
 from hoshino.typing import CQEvent
 import requests
-import json
-from requests import get
-import time
-import re
 se = requests.session()
 
 
 sv = Service("网易云下载")
+
+
 @sv.on_prefix(('网易云下载'))
 async def yxh(bot, ev: CQEvent):
     kw = ev.message.extract_plain_text().strip()
     arr = kw.split('/')
     url = 'https://v1.alapi.cn/api/music/url?id=' + str(arr[0]) + '&format=json'
-    r=requests.get(url)
-    song_info=r.json()
+    r = requests.get(url)
+    song_info = r.json()
     url = song_info["data"]["url"]
     encodeType = song_info["data"]["encodeType"]
-    print (url)
-    msg = "这首歌的下载链接是\n" + str(url) + " \n格式为:320Kbps " + str(encodeType)
-    print (msg)
+    sv.logger.info('Download Link: ' + url)
+    msg = "下载链接：\n" + str(url) + " \n格式：320Kbps " + str(encodeType)
     await bot.send(ev, msg)
-    
