c8300040a33367bef2e71f855297c43b70008357
haha114514 <47912037+haha114514@users.noreply.github.com>
2021-09-10 18:00:20 +1000

    Update neteasedown.py
    修复失效api，完善报错

--- neteasedown.py
+++ neteasedown.py
@@ -1,21 +1,24 @@
 from hoshino import Service
 from hoshino.typing import CQEvent
 import requests
-se = requests.session()
-
-
 sv = Service("网易云下载")
 
+api= 'https://v2.alapi.cn/api/music/url'
+token = 'Your_api_token' #请前往http://alapi.cn申请
 
 @sv.on_prefix(('网易云下载'))
-async def yxh(bot, ev: CQEvent):
+async def netease_down(bot, ev: CQEvent):
     kw = ev.message.extract_plain_text().strip()
-    arr = kw.split('/')
-    url = 'https://v.alapi.cn/api/music/url?id=' + str(arr[0]) + '&format=json'
-    r = requests.get(url)
-    song_info = r.json()
-    url = song_info["data"]["url"]
-    encodeType = song_info["data"]["encodeType"]
-    sv.logger.info('Download Link: ' + url)
-    msg = "下载链接：\n" + str(url) + " \n格式：320Kbps " + str(encodeType)
-    await bot.send(ev, msg)
+    url = f'{api}?id={kw}&format=json&token={token}'
+    song_info = requests.get(url).json()
+    if song_info["code"] == 422:
+        await bot.finish(ev, song_info["msg"] )
+    elif song_info["code"] == 200:
+        url = song_info["data"]["url"]
+        encodeType = song_info["data"]["encodeType"]
+        sv.logger.info('Download Link: ' + url)
+        msg = f"下载链接：\n{url}\n格式：320Kbps{encodeType}"
+        await bot.finish(ev, msg)
+    else:
+        await bot.finish(ev, "未知错误")
+    
